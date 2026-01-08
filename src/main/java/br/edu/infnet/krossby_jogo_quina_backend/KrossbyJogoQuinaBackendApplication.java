a0ff618873a58ac294a185d0c90b07e4615f9848
Krossby <krosscaal@gmail.com>
2026-01-08 13:13:15 -0300

    commit inicial projeto infnet-quina-backend

---
+++ src/main/java/br/edu/infnet/krossby_jogo_quina_backend/KrossbyJogoQuinaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.krossby_jogo_quina_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrossbyJogoQuinaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrossbyJogoQuinaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/krossby_jogo_quina_backend/KrossbyJogoQuinaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.krossby_jogo_quina_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrossbyJogoQuinaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
