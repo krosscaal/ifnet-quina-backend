839c76d8a0d2e13f759bc883100ffdf7fcdef5c9
Krossby <krosscaal@gmail.com>
2026-01-10 18:31:03 -0300

    fix: testes

---
+++ src/test/java/br/edu/infnet/krossby_jogo_quina_backend/UtilsTest.java
@@ -0,0 +1,19 @@
+package br.edu.infnet.krossby_jogo_quina_backend;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @Test
+    void ordenarArrayListaToStringTest() {
+        List<String> lista = Arrays.asList("10", "02", "30");
+        String resultadoEsperado = "02,10,30";
+        assertEquals(resultadoEsperado, Utils.ordenarArrayListaToString(lista));
+    }
+
+}
