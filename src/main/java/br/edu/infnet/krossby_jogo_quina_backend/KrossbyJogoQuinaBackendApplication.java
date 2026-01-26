9c824d96659db2a5aa9241d9c039b78ebb3203bd
Krossby <krosscaal@gmail.com>
2026-01-26 14:20:12 -0300

    feat: add EnableSpringDataWebSupport para serialização de objetos de paginação

--- src/main/java/br/edu/infnet/krossby_jogo_quina_backend/KrossbyJogoQuinaBackendApplication.java
+++ src/main/java/br/edu/infnet/krossby_jogo_quina_backend/KrossbyJogoQuinaBackendApplication.java
@@ -3,9 +3,11 @@ package br.edu.infnet.krossby_jogo_quina_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO) //Habilitada para serializacão objetos de paginação
 public class KrossbyJogoQuinaBackendApplication {
 
 	public static void main(String[] args) {
