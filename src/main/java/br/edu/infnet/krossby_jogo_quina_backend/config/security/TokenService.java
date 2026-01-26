3179b9bdbad9cf0f59b9edd24644851b5559b4d0
Krossby <krosscaal@gmail.com>
2026-01-26 15:03:20 -0300

    feat: alterando validez de token para 2 horas

--- src/main/java/br/edu/infnet/krossby_jogo_quina_backend/config/security/TokenService.java
+++ src/main/java/br/edu/infnet/krossby_jogo_quina_backend/config/security/TokenService.java
@@ -35,7 +35,7 @@ public class TokenService {
         }
     }
     private Instant gerarExpirationDate(){
-        return LocalDateTime.now().plusMinutes(30).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 
     public String validarToken(String token){
